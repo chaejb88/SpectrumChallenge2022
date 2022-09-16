00e1a6523cac3a9d2d104235e1a2f3617ea6cbcc
SpectrumChallenge2 <91661891+SpectrumChallenge2@users.noreply.github.com>
2022-09-17 01:05:56 +0900

    Update agent.py

--- Agent/agent.py
+++ Agent/agent.py
@@ -3,33 +3,29 @@ import numpy as np
 import itertools
 import random
 import torch
+import torch.nn.functional as F
 from torch import nn
 from torch.distributions.categorical import Categorical
 from pathlib import Path
 
 class Agent:
-    def __init__(self, environment, unit_packet_success_reward, unit_packet_failure_reward, discount_factor, dnn_learning_rate):
+    def __init__(self, environment, discount_factor, dnn_learning_rate):
         super(Agent, self).__init__()
+
         self._env = environment
-        self._unit_packet_success_reward = unit_packet_success_reward
-        self._unit_packet_failure_reward = unit_packet_failure_reward
         self._discount_factor = discount_factor
         self._dnn_learning_rate = dnn_learning_rate
         self._num_freq_channel = 4
         self._max_num_unit_packet = 2
         self._sta_list = list(range(0, 8))
         self._observation = np.zeros(self._num_freq_channel)
-
         self._num_freq_channel_combination = 2 ** self._num_freq_channel - 1
         self._num_action = self._num_freq_channel_combination * self._max_num_unit_packet + 1
         self._freq_channel_combination = [np.where(np.flip(np.array(x)))[0].tolist()
                                           for x in itertools.product((0, 1), repeat=self._num_freq_channel)][1:]
-
-        self._actor = MLP(self._num_freq_channel, self._num_action, [128, 64])
-        self._critic = MLP(self._num_freq_channel, 1, [128, 64])
-
+        self._actor = Actor(input_size=self._num_freq_channel, output_size=self._num_action)
+        self._critic = Critic(input_size=self._num_freq_channel, output_size=1)
         self._eps = 1e-25
-        self._mse = torch.nn.MSELoss()
         self._total_param = list(self._actor.parameters()) + list(self._critic.parameters())
         self._optimizer = torch.optim.Adam(params=self._total_param, lr=self._dnn_learning_rate)
         self._model_path = str(Path(__file__).parent.resolve() / 'my_model')
@@ -42,10 +38,13 @@ class Agent:
     def train(self, num_episode, run_time):
         self._env.disable_video_logging()
         self._env.disable_text_logging()
+        print("Train start")
         for episode in range(num_episode):
             self._env.start_simulation(time_us=run_time)
             self.set_init()
-            self.make_memory()
+            loss_avg, reward_avg = self.train_episode()
+            print(f"{episode} episode => loss_avg: {loss_avg}, reward_avg: {reward_avg}")
+        print("finish")
         self.save_model()
 
     def test(self, run_time: int):
@@ -57,51 +56,55 @@ class Agent:
         self.set_init()
         self.load_model()
         while True:
-            action, _, _ = self.get_dnn_action_and_value(self._observation)
-            action_dict = self.convert_action_index_to_dict(action)
+            observation = torch.Tensor(self._observation).type(torch.float32).to(self._device)
+            logits, dist, prob = self._actor(observation)
+            action_index = torch.argmax(prob)
+            action_dict = self.convert_action_index_to_dict(action_index)
             observation_dict = self._env.step(action_dict)
             if observation_dict == 0:
                 break
             self._observation = self.convert_observation_dict_to_arr(observation_dict['observation'])
             print(f"{self._env.get_score()            }\r", end='', flush=True)
 
-    def make_memory(self):
-        done = False
-        while not done:
-            observation_dict = self._env.random_action_step()
-            if observation_dict == {}:
-                return True
-            self._observation = self.convert_observation_dict_to_arr(observation_dict['observation'])
-            action, _, _ = self.get_dnn_action_and_value(self._observation)
-            action_dict = self.convert_action_index_to_dict(action)
+    def train_episode(self):
+        loss_lst = []
+        reward_lst = []
+        while True:
+            observation = torch.Tensor(self._observation).type(torch.float32)
+            logits, dist, probs = self._actor(observation)
+            action_index = torch.argmax(probs)
+            action_dict = self.convert_action_index_to_dict(action_index)
             observation_dict = self._env.step(action_dict)
-            if observation_dict == 0:
-                return True
+            if observation_dict == 0:       # finish episode
+                loss_avg = np.mean(loss_lst)
+                reward_avg = np.mean(reward_lst)
+                return loss_avg, reward_avg
             else:
-                reward = observation_dict['reward']
+                reward = torch.Tensor([observation_dict['reward']]).type(torch.float32)
                 next_observation = self.convert_observation_dict_to_arr(observation_dict['observation'])
-
-                self._observation = torch.Tensor(self._observation)
-                next_observation = torch.Tensor(next_observation)
+                next_observation = torch.Tensor(next_observation).type(torch.float32)
 
                 # compute targets
                 with torch.no_grad():
                     td_target = reward + self._discount_factor * self._critic(next_observation)
-                    td_error = td_target - self._critic(self._observation)
+                    td_error = td_target - self._critic(observation)
 
-                # compute log probabilities
-                dist = Categorical(logits=self._actor(self._observation))
-                prob = dist.probs.gather(0, action.long())
+                # compute actor loss
+                prob = probs[action_index]
+                actor_loss = -torch.log(prob + self._eps) * td_error
+                actor_loss = actor_loss.mean()
+                # compute critic loss
+                v = self._critic(observation)
+                critic_loss = F.mse_loss(v, td_target)
 
-                # compute the values of current states
-                v = self._critic(self._observation)
-
-                loss = -torch.log(prob + self._eps) * td_error + self._mse(v, td_target)
-                loss = loss.mean()
-
+                # backpropagation
+                loss = actor_loss + critic_loss
                 self._optimizer.zero_grad()
                 loss.backward()
                 self._optimizer.step()
+
+                loss_lst.append(loss.item())
+                reward_lst.append(reward.item())
 
     def convert_observation_dict_to_arr(self, observation):
         observation_type = observation['type']
@@ -115,20 +118,7 @@ class Agent:
             observation_arr[success_freq_channel_list] = 3
         return observation_arr
 
-    def get_dnn_action_and_value(self, observation):
-        if observation.ndim == 1:
-            observation = observation[np.newaxis, ...]
-        observation = torch.Tensor(observation)
-
-        with torch.no_grad():
-            logits = self._actor(observation)
-            dist = Categorical(logits=logits)
-            action = dist.sample()  # sample action from softmax policy
-        return action, dist, logits
-
-
     def convert_action_index_to_dict(self, action_index):
-        action_index = action_index[0]
         if action_index == 0:
             action_dict = {'type': 'sensing'}
         else:
@@ -157,41 +147,59 @@ class Agent:
         self._critic.load_state_dict(saved_dict["Critic"])
 
 
-class MLP(nn.Module):
-    def __init__(self, input_size: int, output_size: int, num_neurons: list = [64, 32], hidden_act: str = 'ReLU',
-                 out_act: str = 'Identity'):
-        super(MLP, self).__init__()
+class Actor(nn.Module):
+    def __init__(self, input_size: int, output_size: int, hidden_size: int = 128, num_hidden_layer: int = 2):
+        super().__init__()
         self._input_size = input_size
         self._output_size = output_size
-        self._num_neurons = num_neurons
-        self._hidden_act = getattr(nn, hidden_act)()
-        self._out_act = getattr(nn, out_act)()
+        self._hidden_size = hidden_size
+        self._num_hidden_layer = num_hidden_layer
+        self._act = nn.ReLU()
+        self._input_layer = nn.Linear(self._input_size, self._hidden_size)
+        self._hidden_layer = nn.Linear(self._hidden_size, self._hidden_size)
+        self._output_layer = nn.Linear(self._hidden_size, self._output_size)
 
-        input_sizes = [input_size] + self._num_neurons
-        output_sizes = self._num_neurons + [output_size]
+    def forward(self, x):
+        x = self._input_layer(x)
+        x = self._act(x)
+        for _ in range(self._num_hidden_layer):
+            x = self._hidden_layer(x)
+            x = self._act(x)
+        x = self._output_layer(x)
+        logit = x
+        dist = Categorical(logits=logit)
+        prob = dist.probs
+        return logit, dist, prob
 
-        self._layers = nn.ModuleList()
-        for i, (in_size, out_size) in enumerate(zip(input_sizes, output_sizes)):
-            is_last = True if i == len(input_sizes) - 1 else False
-            self._layers.append(nn.Linear(in_size, out_size))
-            if is_last:
-                self._layers.append(self._out_act)
-            else:
-                self._layers.append(self._hidden_act)
 
-    def forward(self, xs):
-        for layer in self._layers:
-            xs = layer(xs)
-        return xs
+class Critic(nn.Module):
+    def __init__(self, input_size: int, output_size: int, hidden_size: int = 128, num_hidden_layer: int = 2):
+        super().__init__()
+        self._input_size = input_size
+        self._output_size = output_size
+        self._hidden_size = hidden_size
+        self._num_hidden_layer = num_hidden_layer
+        self._act = nn.ReLU()
+        self._input_layer = nn.Linear(self._input_size, self._hidden_size)
+        self._hidden_layer = nn.Linear(self._hidden_size, self._hidden_size)
+        self._output_layer = nn.Linear(self._hidden_size, self._output_size)
+
+    def forward(self, x):
+        x = self._input_layer(x)
+        x = self._act(x)
+        for _ in range(self._num_hidden_layer):
+            x = self._hidden_layer(x)
+            x = self._act(x)
+        x = self._output_layer(x)
+        return x
 
 
 if __name__ == "__main__":
     env = EnvironmentInterface()
     env.connect()
-    agent = Agent(environment=env, unit_packet_success_reward=10, unit_packet_failure_reward=-40, discount_factor=0.9,
-                  dnn_learning_rate=0.00005)
+    agent = Agent(environment=env, discount_factor=0.9, dnn_learning_rate=0.000001)
 
     # When submitting, the training part should be excluded,
     # and it should be submitted in a form that can be evaluated by loading the trained model like agent.test()
-    agent.train(num_episode=10, run_time=100000)
-    agent.test(100000)
+    agent.train(num_episode=1000, run_time=100000)
+    agent.test(500000)
